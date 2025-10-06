e083134903e0762207a12fa2cb4d278157eefb38
rdubovskyi <rdubowskij@gmail.com>
2025-10-06 09:24:06 +0300

    Committed initial files (pom + spring boot dependencies)

---
+++ src/test/java/tech/cusbo/ms_teams_demo/MsTeamsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.cusbo.ms_teams_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTeamsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
